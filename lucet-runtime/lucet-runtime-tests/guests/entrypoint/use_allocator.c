ee46e21d094fd6170e06670c856268b11dd70c53
Pat Hickey <phickey@fastly.com>
2019-03-12 13:21:47 -0700

    lucet-runtime-tests: entrypoint ctype and allocator tests work

--- lucet-runtime/lucet-runtime-tests/guests/entrypoint/use_allocator.c
+++ lucet-runtime/lucet-runtime-tests/guests/entrypoint/use_allocator.c
@@ -1,11 +1,17 @@
 
-#include <assert.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-__attribute__((visibility("default"))) void create_and_memset(int init_as, size_t size,
-                                                              char **ptr_outval)
+// The WASI implementation of assert pulls facilities for in printing to stderr
+// and aborting. This is lighter weight for a unit test
+static void assert(bool v) {
+    if (!v) {
+        __builtin_unreachable();
+    }
+}
+
+void create_and_memset(int init_as, size_t size, char **ptr_outval)
 {
     char *area = malloc(size);
     assert(area);
@@ -13,7 +19,7 @@ __attribute__((visibility("default"))) void create_and_memset(int init_as, size_
     *ptr_outval = area;
 }
 
-__attribute__((visibility("default"))) void increment_ptr(char *ptr)
+void increment_ptr(char *ptr)
 {
     char val = *ptr;
     *ptr     = val + 1;
