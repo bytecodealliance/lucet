754e55612d2907f86d70f7e94b5e34fd5e680623
Pat Hickey <phickey@fastly.com>
2019-03-13 14:34:37 -0700

    delete lucet-libc

--- lucet-libc/src/wasm32_rt/abort.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include "lucet_libc.h"
-
-_Noreturn void abort(void) {
-	lucet_libc_syscall_abort();
-}
--- lucet-libc/src/wasm32_rt/lctrans.c
+++
@@ -1,6 +0,0 @@
-#include "locale_impl.h"
-
-const char *__lctrans(const char *msg, const struct __locale_map *lm) {
-	(void) lm;
-	return msg;
-}
