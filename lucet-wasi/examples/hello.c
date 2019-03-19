e2d696038a645b15315c44f719d290466bcd75f0
Adam C. Foltzer <acfoltzer@fastly.com>
2019-03-19 10:20:37 -0700

    [lucet-wasi] add kgt example

--- lucet-wasi/examples/hello.c
+++ lucet-wasi/examples/hello.c
@@ -1,8 +1,3 @@
-// To compile this code, use a clang toolchain built by
-// [wasmception-wasi](https://github.com/CraneStation/wasmception-wasi/). Because that's a rather
-// involved process, we include the `clang -Os` output for the example in WebAssembly text format
-// (`hello.wat`).
-
 #include <stdio.h>
 #include <stdlib.h>
 
