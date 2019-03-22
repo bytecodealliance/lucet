5bfcc3c7a6a09ce58c07896b790658426c4fccce
Frank Denis <github@pureftpd.org>
2019-03-22 15:33:46 +0100

    Do not run non-shell commands interactively
    This causes build scripts to complain that the tty is not a
    terminal.

--- devenv_run.sh
+++ devenv_run.sh
@@ -25,5 +25,5 @@ fi
 if [ $# -eq 0 ]; then
 	docker exec -it -w "$lucet_workdir" lucet /bin/bash
 else
-	docker exec -it -w "$lucet_workdir" lucet $@
+	docker exec -t -w "$lucet_workdir" lucet $@
 fi
