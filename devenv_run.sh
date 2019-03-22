5bc1d419062cb992bbf9196d4c440ef068756df7
Pat Hickey <phickey@fastly.com>
2019-03-22 15:32:44 -0700

    devenv_run: fix

--- devenv_run.sh
+++ devenv_run.sh
@@ -7,7 +7,7 @@ if ! docker ps -f name=lucet | grep -Fq lucet ; then
 fi
 
 if [ $# -eq 0 ]; then
-	docker exec -it -w "$lucet_workdir" lucet /bin/bash
+	docker exec -it -w "$HOST_LUCET_MOUNT_POINT" lucet /bin/bash
 else
-	docker exec -t -w "$lucet_workdir" lucet $@
+	docker exec -t -w "$HOST_LUCET_MOUNT_POINT" lucet $@
 fi
