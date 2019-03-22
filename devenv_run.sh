8d8e2e84329d19c483869dfb0720b0bf2b8711eb
Frank Denis <github@pureftpd.org>
2019-03-22 14:59:27 +0100

    Map host path to guest path in devenv_run.sh

--- devenv_run.sh
+++ devenv_run.sh
@@ -6,13 +6,24 @@ if ! docker ps -f name=lucet | grep -Fq lucet ; then
 	${HOST_BASE_PREFIX}/devenv_start.sh
 fi
 
-if ! docker exec -t -w /lucet lucet stat "$LUCET_PREFIX" > /dev/null ; then
+lucet_workdir="$HOST_LUCET_MOUNT_POINT"
+prefix="$(pwd)"
+relpath=""
+while [ -n "$prefix" -a "$prefix" != "/" -a "$prefix" != "$HOST_BASE_PREFIX" ]; do
+	relpath="$(basename $prefix)/${relpath}"
+	prefix=$(dirname "$prefix")
+done
+if [ "$prefix" = "$HOST_BASE_PREFIX" ]; then
+	lucet_workdir="${HOST_LUCET_MOUNT_POINT}/${relpath}"
+fi
+
+if ! docker exec -t -w "$lucet_workdir" lucet stat "$LUCET_PREFIX" > /dev/null ; then
 	echo "Lucet hasn't been installed yet... installing..."
-	docker exec -t -w /lucet lucet make install
+	docker exec -t -w "$lucet_workdir" lucet make install
 fi
 
 if [ $# -eq 0 ]; then
-	docker exec -it -w /lucet lucet /bin/bash
+	docker exec -it -w "$lucet_workdir" lucet /bin/bash
 else
-	docker exec -it -w /lucet lucet $@
+	docker exec -it -w "$lucet_workdir" lucet $@
 fi
