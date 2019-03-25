5add3beafedba3bf6a940d28795242a57d050b97
Frank Denis <github@pureftpd.org>
2019-03-25 23:52:17 +0100

    Make devenv_run.sh accept any relative paths again

--- devenv_run.sh
+++ devenv_run.sh
@@ -6,8 +6,19 @@ if ! docker ps -f name=lucet | grep -Fq lucet ; then
 	${HOST_BASE_PREFIX}/devenv_start.sh
 fi
 
+lucet_workdir="$HOST_LUCET_MOUNT_POINT"
+prefix="$(pwd)"
+relpath=""
+while [ -n "$prefix" -a "$prefix" != "/" -a "$prefix" != "$HOST_BASE_PREFIX" ]; do
+	relpath="$(basename $prefix)/${relpath}"
+	prefix=$(dirname "$prefix")
+done
+if [ "$prefix" = "$HOST_BASE_PREFIX" ]; then
+	lucet_workdir="${HOST_LUCET_MOUNT_POINT}/${relpath}"
+fi
+
 if [ $# -eq 0 ]; then
-	docker exec -it -w "$HOST_LUCET_MOUNT_POINT" lucet /bin/bash
+	docker exec -it -w "$lucet_workdir" lucet /bin/bash
 else
-	docker exec -t -w "$HOST_LUCET_MOUNT_POINT" lucet $@
+	docker exec -t -w "$lucet_workdir" lucet $@
 fi
