b09d8301d568b63584a6441254f95c0f9e130b4c
Pat Hickey <phickey@fastly.com>
2019-04-25 12:03:36 -0700

    lucet-idl: separate command line opt parsing from library config

--- lucet-idl/src/config.rs
+++ lucet-idl/src/config.rs
@@ -1,78 +1,27 @@
-use super::backend::*;
-use super::errors::*;
-use super::target::*;
-use clap::{App, Arg};
-use std::path::PathBuf;
+use super::backend::{Backend, BackendConfig};
+use super::target::Target;
 
 #[derive(Default, Clone, Debug)]
 pub struct Config {
-    pub input_path: PathBuf,
-    pub output_path: Option<PathBuf>,
     pub target: Target,
     pub backend: Backend,
     pub backend_config: BackendConfig,
 }
 
 impl Config {
-    pub fn parse() -> Result<Self, IDLError> {
-        let matches = App::new("lucet-idl")
-            .version("1.0")
-            .about("lucet_idl code generator")
-            .arg(
-                Arg::with_name("input_file")
-                    .short("i")
-                    .long("input")
-                    .takes_value(true)
-                    .required(true)
-                    .help("Path to the input file"),
-            )
-            .arg(
-                Arg::with_name("target")
-                    .short("t")
-                    .long("target")
-                    .default_value("Generic")
-                    .takes_value(true)
-                    .required(false)
-                    .help("Target, one of: x86, x86_64, x86_64_32, generic"),
-            )
-            .arg(
-                Arg::with_name("backend")
-                    .short("b")
-                    .long("backend")
-                    .default_value("c")
-                    .takes_value(true)
-                    .required(false)
-                    .help("Backend, one of: c, rust"),
-            )
-            .arg(
-                Arg::with_name("zero-native-pointers")
-                    .short("z")
-                    .long("zero-native-pointers")
-                    .takes_value(false)
-                    .required(false)
-                    .help("Do not serialize native pointers"),
-            )
-            .get_matches();
-        let input_path = PathBuf::from(
-            matches
-                .value_of("input_file")
-                .ok_or(IDLError::UsageError("Input file required"))?,
-        );
-        let mut target = Target::from(matches.value_of("target").unwrap());
-        let backend = Backend::from(matches.value_of("backend").unwrap());
-        let zero_native_pointers = matches.is_present("zero-native-pointers");
+    pub fn parse(target_opt: &str, backend_opt: &str, zero_native_pointers: bool) -> Self {
+        let mut target = Target::from(target_opt);
+        let backend = Backend::from(backend_opt);
         if zero_native_pointers {
             target = Target::Generic;
         }
         let backend_config = BackendConfig {
             zero_native_pointers,
         };
-        Ok(Config {
-            input_path,
-            output_path: None,
+        Self {
             target,
             backend,
             backend_config,
-        })
+        }
     }
 }
--- lucet-idl/src/main.rs
+++ lucet-idl/src/main.rs
@@ -24,11 +24,76 @@ use lucet_idl::validate::*;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use clap::{App, Arg};
+use std::path::PathBuf;
 
+
+#[derive(Default, Clone, Debug)]
+pub struct ExeConfig {
+    pub input_path: PathBuf,
+    pub output_path: Option<PathBuf>,
+    pub config: Config,
+}
+
+impl ExeConfig {
+    pub fn parse() -> Result<Self, IDLError> {
+        let matches = App::new("lucet-idl")
+            .version("1.0")
+            .about("lucet_idl code generator")
+            .arg(
+                Arg::with_name("input_file")
+                    .short("i")
+                    .long("input")
+                    .takes_value(true)
+                    .required(true)
+                    .help("Path to the input file"),
+            )
+            .arg(
+                Arg::with_name("target")
+                    .short("t")
+                    .long("target")
+                    .default_value("Generic")
+                    .takes_value(true)
+                    .required(false)
+                    .help("Target, one of: x86, x86_64, x86_64_32, generic"),
+            )
+            .arg(
+                Arg::with_name("backend")
+                    .short("b")
+                    .long("backend")
+                    .default_value("c")
+                    .takes_value(true)
+                    .required(false)
+                    .help("Backend, one of: c, rust"),
+            )
+            .arg(
+                Arg::with_name("zero-native-pointers")
+                    .short("z")
+                    .long("zero-native-pointers")
+                    .takes_value(false)
+                    .required(false)
+                    .help("Do not serialize native pointers"),
+            )
+            .get_matches();
+        let input_path = PathBuf::from(
+            matches
+                .value_of("input_file")
+                .ok_or(IDLError::UsageError("Input file required"))?,
+        );
+        let config = Config::parse(matches.value_of("target").unwrap(),
+                                    matches.value_of("backend").unwrap(),
+        matches.is_present("zero-native-pointers"));
+        Ok(ExeConfig {
+            input_path,
+            output_path: None,
+            config,
+        })
+    }
+}
 fn doit() -> Result<(), IDLError> {
-    let config = Config::parse()?;
+    let exe_config = ExeConfig::parse()?;
     let mut source = String::new();
-    File::open(&config.input_path)?.read_to_string(&mut source)?;
+    File::open(&exe_config.input_path)?.read_to_string(&mut source)?;
     let mut parser = Parser::new(&source);
     let decls = parser.match_decls()?;
     let data_description = DataDescription::validate(&decls)?;
@@ -37,7 +102,7 @@ fn doit() -> Result<(), IDLError> {
         .map_err(|_| IDLError::InternalError("Unable to resolve dependencies"))?;
     let data_description_helper = DataDescriptionHelper { data_description };
     let mut cache = Cache::default();
-    let mut generator = Generators::c(&config);
+    let mut generator = Generators::c(&exe_config.config);
     let mut pretty_writer = PrettyWriter::new(io::stdout());
     generator.gen_prelude(&mut pretty_writer)?;
     for id in deps {
