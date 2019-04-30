4166def13b9f273c5da89bb1c4859541c3fde566
Pat Hickey <phickey@fastly.com>
2019-04-30 11:41:36 -0700

    lucet-idl: start stubbing out rust backend

--- lucet-idl/src/rust/mod.rs
+++ lucet-idl/src/rust/mod.rs
@@ -1,14 +1,15 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use crate::backend::*;
-use crate::cache::*;
-use crate::errors::*;
+use crate::backend::BackendConfig;
+use crate::cache::Cache;
+use crate::errors::IDLError;
 use crate::generator::{Generator, Hierarchy};
-use crate::module::{DataTypeEntry, DataTypeRef, Module};
-use crate::pretty_writer::*;
-use crate::target::*;
-use std::io::prelude::*;
+use crate::module::{DataTypeEntry, DataTypeRef, DataType, Module};
+use crate::pretty_writer::PrettyWriter;
+use crate::target::Target;
+use std::io::Write;
+use heck::CamelCase;
 
 #[derive(Clone, Debug)]
 struct CTypeInfo<'t> {
@@ -31,8 +32,8 @@ pub struct RustGenerator {
 }
 
 impl<W: Write> Generator<W> for RustGenerator {
-    fn gen_prelude(&mut self, pretty_writer: &mut PrettyWriter<W>) -> Result<(), IDLError> {
-        unimplemented!()
+    fn gen_prelude(&mut self, _pretty_writer: &mut PrettyWriter<W>) -> Result<(), IDLError> {
+        Ok(())
     }
 
     fn gen_type_header(
@@ -42,7 +43,12 @@ impl<W: Write> Generator<W> for RustGenerator {
         pretty_writer: &mut PrettyWriter<W>,
         data_type_entry: &DataTypeEntry<'_>,
     ) -> Result<(), IDLError> {
-        unimplemented!()
+        pretty_writer
+            .eob()?
+            .write_line(
+                format!("/// {}: {:?}", data_type_entry.name.name, data_type_entry).as_bytes(),
+            )?;
+        Ok(())
     }
 
     // The most important thing in alias generation is to cache the size
@@ -54,7 +60,17 @@ impl<W: Write> Generator<W> for RustGenerator {
         pretty_writer: &mut PrettyWriter<W>,
         data_type_entry: &DataTypeEntry<'_>,
     ) -> Result<(), IDLError> {
-        unimplemented!()
+        let (pointee, _attrs) = if let DataType::Alias { to: pointee, attrs } = &data_type_entry.data_type {
+            (pointee, attrs)
+        } else {
+            unreachable!()
+        };
+        pretty_writer
+            .write_line(
+                format!("type {} = {:?};", data_type_entry.name.name.to_camel_case(), pointee).as_bytes(),
+            )?
+            .eob()?;
+        Ok(())
     }
 
     fn gen_struct(
@@ -64,7 +80,18 @@ impl<W: Write> Generator<W> for RustGenerator {
         pretty_writer: &mut PrettyWriter<W>,
         data_type_entry: &DataTypeEntry<'_>,
     ) -> Result<(), IDLError> {
-        unimplemented!()
+        pretty_writer
+            .write_line(
+                "#[repr(C)]".as_bytes()
+            )?
+            .write_line(
+                format!("struct {} {{", data_type_entry.name.name.to_camel_case()).as_bytes(),
+            )?
+            .write_line(
+                "}".as_bytes(),
+            )?
+            .eob()?;
+        Ok(())
     }
 
     // Enums generate both a specific typedef, and a traditional C-style enum
@@ -76,39 +103,70 @@ impl<W: Write> Generator<W> for RustGenerator {
         pretty_writer: &mut PrettyWriter<W>,
         data_type_entry: &DataTypeEntry<'_>,
     ) -> Result<(), IDLError> {
-        unimplemented!()
+        let (named_members, _attrs) = if let DataType::Enum {
+            members: named_members,
+            attrs,
+        } = &data_type_entry.data_type
+        {
+            (named_members, attrs)
+        } else {
+            unreachable!()
+        };
+
+
+        pretty_writer
+            .write_line(
+                "#[repr(C)]".as_bytes()
+            )?
+            .write_line(
+                "#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]".as_bytes()
+            )?
+            .write_line(
+                format!("enum {} {{", data_type_entry.name.name.to_camel_case()).as_bytes(),
+            )?;
+
+        for m in named_members {
+            pretty_writer.write_line(format!("    {},", m.name.to_camel_case()).as_bytes())?;
+        }
+
+        pretty_writer
+            .write_line(
+                "}".as_bytes(),
+            )?
+            .eob()?;
+        Ok(())
     }
 
     fn gen_accessors_struct(
         &mut self,
-        module: &Module,
-        cache: &Cache,
-        pretty_writer: &mut PrettyWriter<W>,
-        data_type_entry: &DataTypeEntry<'_>,
-        hierarchy: &Hierarchy,
+        _module: &Module,
+        _cache: &Cache,
+        _pretty_writer: &mut PrettyWriter<W>,
+        _data_type_entry: &DataTypeEntry<'_>,
+        _hierarchy: &Hierarchy,
     ) -> Result<(), IDLError> {
-        unimplemented!()
+        Ok(())
     }
 
     fn gen_accessors_enum(
         &mut self,
-        module: &Module,
-        cache: &Cache,
-        pretty_writer: &mut PrettyWriter<W>,
-        data_type_entry: &DataTypeEntry<'_>,
-        hierarchy: &Hierarchy,
+        _module: &Module,
+        _cache: &Cache,
+        _pretty_writer: &mut PrettyWriter<W>,
+        _data_type_entry: &DataTypeEntry<'_>,
+        _hierarchy: &Hierarchy,
     ) -> Result<(), IDLError> {
-        unimplemented!()
+        Ok(())
     }
 
     fn gen_accessors_alias(
         &mut self,
-        module: &Module,
-        cache: &Cache,
-        pretty_writer: &mut PrettyWriter<W>,
-        data_type_entry: &DataTypeEntry<'_>,
-        hierarchy: &Hierarchy,
+        _module: &Module,
+        _cache: &Cache,
+        _pretty_writer: &mut PrettyWriter<W>,
+        _data_type_entry: &DataTypeEntry<'_>,
+        _hierarchy: &Hierarchy,
     ) -> Result<(), IDLError> {
-        unimplemented!()
+        Ok(())
     }
 }
