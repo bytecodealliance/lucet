fe35b38dd6c3ca6f8bc3c76878f5d9cfabe4c196
Frank Denis <github@pureftpd.org>
2019-05-06 19:39:51 +0200

    Implement the remaining hostcalls

--- lucet-wasi/tests/tests.rs
+++ lucet-wasi/tests/tests.rs
@@ -378,3 +378,18 @@ fn stat() {
     let exitcode = run("stat.c", ctx).unwrap();
     assert_eq!(exitcode, 0);
 }
+
+#[test]
+fn fs() {
+    let tmpdir = TempDir::new().unwrap();
+    let preopen_host_path = tmpdir.path().join("preopen");
+    std::fs::create_dir(&preopen_host_path).unwrap();
+    let preopen_dir = File::open(&preopen_host_path).unwrap();
+    let ctx = WasiCtxBuilder::new()
+        .args(&["stat"])
+        .preopened_dir(preopen_dir, "/sandbox")
+        .build()
+        .expect("can build WasiCtx");
+    let exitcode = run("fs.c", ctx).unwrap();
+    assert_eq!(exitcode, 0);
+}
