d269627145bdc373a4dd58608f57541cf0a4a456
Frank Denis <github@pureftpd.org>
2019-06-25 16:58:41 +0200

    Indent all the lucet subprojects

--- helpers/indent.sh
+++ helpers/indent.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 set -e
 ARG=$1
-cleanup () {
+cleanup() {
 	if [[ $ARG == "check" ]]; then
 		echo ""
 		echo "Formatting diffs detected! run \"./indent\" to correct."
 	fi
 	rm -f .formatted
 }
-trap cleanup 1 2 3 6 9 15
+trap cleanup 1 2 3 6 15
 
-if ! $(rustfmt --version | grep -q "rustfmt 1.2.0-stable"); then
+if ! rustfmt --version | grep -q "rustfmt 1.2.0-stable"; then
 	echo "indent requires rustfmt 1.2.0-stable"
-	exit 1;
+	exit 1
 fi
 
-RUST_DIRS=$(find lucet-analyze lucet-idl lucet-spectest lucetc lucet-runtime lucet-wasi-sdk -type f -name 'Cargo.toml' -print)
+RUST_DIRS=$(find lucetc lucet-* benchmarks/lucet-* -type f -name 'Cargo.toml' -print)
 
 if [[ $ARG == "check" ]]; then
 	for RUST_DIR in ${RUST_DIRS}; do
-		pushd $(dirname ${RUST_DIR}) > /dev/null
+		pushd "$(dirname ${RUST_DIR})" >/dev/null
 		cargo fmt -- --check
-		popd > /dev/null
+		popd >/dev/null
 	done
 elif [[ $ARG == "" ]]; then
 	for RUST_DIR in ${RUST_DIRS}; do
-		pushd $(dirname ${RUST_DIR}) > /dev/null
+		pushd "$(dirname ${RUST_DIR})" >/dev/null
 		cargo fmt
-		popd > /dev/null
+		popd >/dev/null
 	done
 else
 	echo "unsupported argument: $1"
 	exit 1
 fi
-
