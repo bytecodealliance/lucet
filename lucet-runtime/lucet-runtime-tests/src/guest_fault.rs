d1bd475c201187220b30f74600a03e6457228e53
Andy Wortman <awortman@fastly.com>
2019-09-17 17:55:18 -0700

    rephrase test to be more run-friendly
    this allows guest_fault to pass .... if run with --test-threads=1
    
    i think manually setting an alarm is too low-level to play well with our
    timeout mechanics, and we'll have to just not do that (nor support it)

--- lucet-runtime/lucet-runtime-tests/src/guest_fault.rs
+++ lucet-runtime/lucet-runtime-tests/src/guest_fault.rs
@@ -128,7 +128,7 @@ macro_rules! guest_fault_tests {
         use lucet_runtime::vmctx::{lucet_vmctx, Vmctx};
         use lucet_runtime::{
             lucet_hostcall_terminate, lucet_hostcalls, DlModule, Error, FaultDetails, Instance,
-            Limits, Region, SignalBehavior, TrapCode,
+            Limits, Region, SignalBehavior, TerminationDetails, TrapCode,
         };
         use nix::sys::mman::{mmap, MapFlags, ProtFlags};
         use nix::sys::signal::{sigaction, SaFlags, SigAction, SigHandler, SigSet, Signal};
@@ -457,10 +457,6 @@ macro_rules! guest_fault_tests {
 
         #[test]
         fn alarm() {
-            extern "C" fn timeout_handler(signum: libc::c_int) {
-                assert!(signum == libc::SIGALRM);
-                std::process::exit(3);
-            }
             test_ex(|| {
                 let module = mock_traps_module();
                 let region =
@@ -471,36 +467,17 @@ macro_rules! guest_fault_tests {
 
                 inst.set_fatal_handler(fatal_handler_exit);
 
-                match fork().expect("can fork") {
-                    ForkResult::Child => {
-                        // set up alarm handler and pend an alarm in 1 second
-                        unsafe {
-                            // child process doesn't have any contention for installed signal handlers, so
-                            // we don't need to grab the lock exclusively here
-                            sigaction(
-                                Signal::SIGALRM,
-                                &SigAction::new(
-                                    SigHandler::Handler(timeout_handler),
-                                    SaFlags::empty(),
-                                    SigSet::empty(),
-                                ),
-                            )
-                            .expect("sigaction succeeds");
-                        }
-                        nix::unistd::alarm::set(1);
+                nix::unistd::alarm::set(1);
 
-                        // run guest code that loops forever
-                        inst.run("infinite_loop", &[]).expect("instance runs");
-                        // show that we never get here
-                        std::process::exit(1);
+                // run guest code that loops forever. this will recieve SIGALRM in one second.
+                let res = inst.run("infinite_loop", &[]);
+
+                match res {
+                    Err(Error::RuntimeTerminated(TerminationDetails::Remote)) => {
+                        // this is what we're looking for! so we're good.
                     }
-                    ForkResult::Parent { child } => {
-                        match waitpid(Some(child), None).expect("can wait on child") {
-                            WaitStatus::Exited(_, code) => {
-                                assert_eq!(code, 3);
-                            }
-                            ws => panic!("unexpected wait status: {:?}", ws),
-                        }
+                    e => {
+                        panic!("Unexpected run result: {:?}", e);
                     }
                 }
             })
