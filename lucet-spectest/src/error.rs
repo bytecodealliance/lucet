f5b6533033fdf50efe72644905cc5f6358c3165a
Tanya L. Crenshaw <tcrenshaw@fastly.com>
2020-01-14 23:35:59 +0000

    Remove unused code.

--- lucet-spectest/src/error.rs
+++ lucet-spectest/src/error.rs
@@ -1,47 +1,4 @@
-//use std::fmt::{self, Display};
 use thiserror::Error;
-
-/*
-#[derive(Debug)]
-pub struct SpecTestError {
-    inner: Context<SpecTestErrorKind>,
-}
-
-impl From<Context<SpecTestErrorKind>> for SpecTestError {
-    fn from(inner: Context<SpecTestErrorKind>) -> SpecTestError {
-        SpecTestError { inner }
-    }
-}
-
-impl From<SpecTestErrorKind> for SpecTestError {
-    fn from(kind: SpecTestErrorKind) -> SpecTestError {
-        SpecTestError {
-            inner: Context::new(kind),
-        }
-    }
-}
-
-impl SpecTestError {
-    pub fn get_context(&self) -> &SpecTestErrorKind {
-        self.inner.get_context()
-    }
-}
-
-impl Fail for SpecTestError {
-    fn cause(&self) -> Option<&dyn Fail> {
-        self.inner.cause()
-    }
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.inner.backtrace()
-    }
-}
-
-impl Display for SpecTestError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        Display::fmt(&self.inner, f)
-    }
-}
- */
 
 #[derive(Debug, Error)]
 pub enum Error {
