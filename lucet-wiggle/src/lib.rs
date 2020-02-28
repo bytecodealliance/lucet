16646090430e4674359fd80114d304349a3ce2af
Pat Hickey <pat@moreproductive.org>
2020-02-28 11:55:27 -0800

    lucet-wiggle: initial stub

---
+++ lucet-wiggle/src/lib.rs
@@ -0,0 +1,9 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use syn::parse_macro_input;
+
+#[proc_macro]
+pub fn from_witx(args: TokenStream) -> TokenStream {
+    let config = parse_macro_input!(args as wiggle_generate::Config);
+    TokenStream::from(wiggle_generate::generate(config))
+}
