144e82fe727286b05e4933a60f7459a5160bf2f8
Pat Hickey <pat@moreproductive.org>
2020-03-11 14:51:44 -0700

    wiggle: get it from wasmtime, with relative path fix

--- lucet-wiggle/src/lib.rs
+++ lucet-wiggle/src/lib.rs
@@ -4,8 +4,12 @@ use syn::parse_macro_input;
 
 #[proc_macro]
 pub fn from_witx(args: TokenStream) -> TokenStream {
-    let config = parse_macro_input!(args as lucet_wiggle_generate::Config);
-    let doc = witx::load(&config.wiggle.witx.paths).expect("loading witx");
+    let mut config = parse_macro_input!(args as lucet_wiggle_generate::Config);
+    config.wiggle.witx.make_paths_relative_to(
+        std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANFIEST_DIR env var"),
+    );
+
+    let doc = witx::load(&config.wiggle.witx.paths).expect("lucet loading witx");
 
     let mut ts = wiggle_generate::generate(&doc, &config.wiggle);
     ts.extend(lucet_wiggle_generate::generate(&doc, &config));
