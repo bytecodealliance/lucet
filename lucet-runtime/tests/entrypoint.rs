5abad26d9d6e78d4ddf476474bac9f2aaca83fe4
Tyler McMullen <tyler@fastly.com>
2020-04-01 10:07:18 -0700

    Start converting tests to accept multiple Region implementations. Starting with entrypoint_test.

--- lucet-runtime/tests/entrypoint.rs
+++ lucet-runtime/tests/entrypoint.rs
@@ -1,3 +1,6 @@
 use lucet_runtime_tests::entrypoint_tests;
 
-entrypoint_tests!(lucet_runtime::MmapRegion);
+entrypoint_tests!(
+    lucet_runtime::MmapRegion => mmap,
+    lucet_runtime::UffdRegion => uffd
+);
