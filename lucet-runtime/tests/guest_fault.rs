595da2eaa7158ea583e4d634990a7acf208c7ea2
Tyler McMullen <tyler@fastly.com>
2020-04-01 16:42:33 -0700

    Make the guest_fault test suite work with UffdRegion. Included fixing a fork-related bug.

--- lucet-runtime/tests/guest_fault.rs
+++ lucet-runtime/tests/guest_fault.rs
@@ -2,4 +2,7 @@ use lucet_runtime_tests::{guest_fault_common_defs, guest_fault_tests};
 
 guest_fault_common_defs!();
 
-guest_fault_tests!(lucet_runtime::MmapRegion);
+guest_fault_tests!(
+    mmap => lucet_runtime::MmapRegion,
+    uffd => lucet_runtime::UffdRegion
+);
