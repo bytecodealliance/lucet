bb0554f7ed40953843da67f21e597f4ca2dec712
Adam C. Foltzer <acfoltzer@fastly.com>
2020-04-10 14:26:04 -0700

    Enable the `uffd` feature by default, but filter on OS
    Notably, this should get us building and running uffd in Linux CI.
    
    It turns out to be a tremendous pain to enable a feature flag for just one crate within a
    workspace. The situation is [being addressed][1], but in the meantime I believe the best route
    forward is to just have uffd on by default for Linux.
    
    [1]: https://github.com/rust-lang/cargo/issues/5364

--- lucet-runtime/tests/guest_fault.rs
+++ lucet-runtime/tests/guest_fault.rs
@@ -3,7 +3,7 @@ use lucet_runtime_tests::{guest_fault_common_defs, guest_fault_tests};
 guest_fault_common_defs!();
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "uffd")] {
+    if #[cfg(all(target_os = "linux", feature = "uffd"))] {
         guest_fault_tests!(
             mmap => lucet_runtime::MmapRegion,
             uffd => lucet_runtime::UffdRegion
--- lucet-runtime/tests/start.rs
+++ lucet-runtime/tests/start.rs
@@ -1,7 +1,7 @@
 use lucet_runtime_tests::start_tests;
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "uffd")] {
+    if #[cfg(all(target_os = "linux", feature = "uffd"))] {
         start_tests!(
             mmap => lucet_runtime::MmapRegion,
             uffd => lucet_runtime::UffdRegion
