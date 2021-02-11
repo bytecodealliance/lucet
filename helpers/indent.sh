8c70344c0405299b204f8172c7a6763ba0e8a2f0
Pat Hickey <phickey@fastly.com>
2021-02-11 14:04:39 -0800

    use rust 1.50 (#629)
    * use rust 1.50
    
    * indent script

--- helpers/indent.sh
+++ helpers/indent.sh
@@ -10,8 +10,8 @@ cleanup() {
 }
 trap cleanup 1 2 3 6 15
 
-# rust 1.48.0 ships with rustfmt 1.4.24-stable
-RUSTFMT_VERSION=1.4.24-stable
+# rust 1.50.0 ships with rustfmt 1.4.30-stable
+RUSTFMT_VERSION=1.4.30-stable
 
 if ! rustfmt --version | grep -q "rustfmt $RUSTFMT_VERSION"; then
 	echo "indent requires rustfmt $RUSTFMT_VERSION"
