a13c008dc07b40cffa789e2dc35635e65a827690
Pat Hickey <pat@moreproductive.org>
2021-05-10 17:10:27 -0700

    lucet-runtime-tests: test that a vmctx::block_on can still access memory through vmctx
    This test fails on this commit, will be fixed by the next one

--- lucet-runtime/lucet-runtime-tests/guests/async_hostcall/hostcall_block_on.c
+++ lucet-runtime/lucet-runtime-tests/guests/async_hostcall/hostcall_block_on.c
@@ -1,9 +1,19 @@
 #include <stddef.h>
+#include <stdint.h>
+#include <assert.h>
 
 extern void hostcall_containing_block_on(int);
+extern void hostcall_block_on_access_vmctx(uint8_t*, uint8_t);
+
+static uint8_t some_byte = 0;
 
 int main(void)
 {
     hostcall_containing_block_on(1312);
-    return 0;
+    hostcall_block_on_access_vmctx(&some_byte, 1);
+    if (some_byte == 1) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
