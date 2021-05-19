0756fbda85cd19ad2e0169191ccd692f5302aef0
Pat Hickey <pat@moreproductive.org>
2021-05-19 10:27:44 -0700

    wasi-common build.rs: use CARGO_MANIFEST_DIR instead of cwd

--- wasmtime-new-api/crates/wasi-common/build.rs
+++ wasmtime-new-api/crates/wasi-common/build.rs
@@ -1,7 +1,7 @@
 // Tell any dependencies, if necessary, where our WASI submodule is so they can
 // use the same witx files if they want.
 fn main() {
-    let cwd = std::env::current_dir().unwrap();
+    let cwd = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     let wasi = cwd
         .parent()
         .unwrap()
