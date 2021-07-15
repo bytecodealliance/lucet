d49c47905d1a9ebb368f7ee1ce635e21a2b00884
Pat Hickey <phickey@fastly.com>
2021-07-15 11:19:36 -0700

    lucet-runtime-example: fix regression from #655 (#664)
    closes #663

--- docs/lucet-runtime-example/src/main.rs
+++ docs/lucet-runtime-example/src/main.rs
@@ -1,7 +1,8 @@
 use lucet_runtime::{DlModule, Limits, MmapRegion, Region};
 use lucet_wasi::WasiCtxBuilder;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // ensure the WASI symbols are exported from the final executable
     lucet_wasi::export_wasi_funcs();
     // load the compiled Lucet module
@@ -10,9 +11,10 @@ fn main() {
     let region = MmapRegion::create(1, &Limits::default()).unwrap();
     // instantiate the module in the memory region
     let mut instance = region.new_instance(dl_module).unwrap();
-    // prepare the WASI context, inheriting stdio handles from the host executable
+    // prepare the WASI context, inheriting stdio handles from the host executable.
+    // Since we are using lucet-wasi, we need to run the instance as async!
     let wasi_ctx = WasiCtxBuilder::new().inherit_stdio().build();
     instance.insert_embed_ctx(wasi_ctx);
-    // run the WASI main function
-    instance.run("main", &[]).unwrap();
+    // run the WASI entrypoint.
+    instance.run_async("_start", &[], None).await.unwrap();
 }
